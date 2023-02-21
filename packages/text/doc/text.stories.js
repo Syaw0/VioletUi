494a781698fd232e03060155b07f74497daf227d
Siavash <siamhb7@protonmail.com>
2023-02-21 20:42:26 +0330

    work on the typography package.

--- packages/text/doc/text.stories.js
+++ packages/text/doc/text.stories.js
@@ -3,4 +3,34 @@ import { Text } from "../lib/text";
 
 export default { title: "Text" };
 
-export const small = () => <Text>Hello Text</Text>;
+export const bodySmall = () => <Text variant="bodySmall">Hello Text</Text>;
+export const bodyLarge = () => <Text variant="bodyLarge">Hello Text</Text>;
+export const bodyMedium = () => <Text variant="bodyMedium">Hello Text</Text>;
+
+export const displayLarge = () => (
+  <Text variant="displayLarge">Hello Text</Text>
+);
+export const displayMedium = () => (
+  <Text variant="displayMedium">Hello Text</Text>
+);
+export const displaySmall = () => (
+  <Text variant="displaySmall">Hello Text</Text>
+);
+
+export const headlineLarge = () => (
+  <Text variant="headlineLarge">Hello Text</Text>
+);
+export const headlineMedium = () => (
+  <Text variant="headlineMedium">Hello Text</Text>
+);
+export const headlineSmall = () => (
+  <Text variant="headlineSmall">Hello Text</Text>
+);
+
+export const labelLarge = () => <Text variant="labelLarge">Hello Text</Text>;
+export const labelMedium = () => <Text variant="labelMedium">Hello Text</Text>;
+export const labelSmall = () => <Text variant="labelSmall">Hello Text</Text>;
+
+export const titleLarge = () => <Text variant="titleLarge">Hello Text</Text>;
+export const titleMedium = () => <Text variant="titleMedium">Hello Text</Text>;
+export const titleSmall = () => <Text variant="titleSmall">Hello Text</Text>;
--- packages/text/lib/text.tsx
+++ packages/text/lib/text.tsx
@@ -1,5 +1,44 @@
 import style from "./text.module.css";
 import React from "react";
 
-const Text = ({ children }: any) => <p className={style.text}>{children}</p>;
+interface TextProps extends React.ComponentPropsWithoutRef<"p"> {
+  variant?:
+    | "displayLarge"
+    | "displayMedium"
+    | "displaySmall"
+    | "headlineLarge"
+    | "headlineMedium"
+    | "headlineSmall"
+    | "titleLarge"
+    | "titleMedium"
+    | "titleSmall"
+    | "bodyLarge"
+    | "bodyMedium"
+    | "bodySmall"
+    | "labelLarge"
+    | "labelMedium"
+    | "labelSmall";
+  as?: "span" | "p";
+}
+
+const Text = ({
+  children,
+  as = "p",
+  variant = "bodyMedium",
+  className,
+  ...props
+}: TextProps) => {
+  const TypographyComponent = `${as}` as keyof Pick<
+    JSX.IntrinsicElements,
+    "p" | "span"
+  >;
+  return (
+    <TypographyComponent
+      className={`${style.typography} ${style[variant]} ${className}`}
+      {...props}
+    >
+      {children}
+    </TypographyComponent>
+  );
+};
 export { Text };
