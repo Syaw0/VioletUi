76d69a79afdb08101c6574b6589a03ac29f8babd
Siavash <siamhb7@protonmail.com>
2023-02-21 20:24:40 +0330

    work on the button package.

--- packages/button/doc/button.stories.js
+++ packages/button/doc/button.stories.js
@@ -1,6 +1,16 @@
 import React from "react";
 import { Button } from "../lib/button";
+import "../../../t.css";
 
 export default { title: "Button" };
 
-export const primary = () => <Button>Hello Button</Button>;
+export const primary = () => <Button color="primary">Hello Button</Button>;
+export const secondary = () => <Button color="secondary">Hello Button</Button>;
+export const tertiary = () => <Button color="tertiary">Hello Button</Button>;
+export const error = () => <Button color="error">Hello Button</Button>;
+
+export const contained = () => (
+  <Button variant="contained">Hello Button</Button>
+);
+export const outlined = () => <Button variant="outlined">Hello Button</Button>;
+export const shadow = () => <Button variant="shadow">Hello Button</Button>;
--- packages/button/lib/button.tsx
+++ packages/button/lib/button.tsx
@@ -1,7 +1,42 @@
 import React from "react";
 import style from "./button.module.css";
 
-const Button = ({ children }: any) => (
-  <button className={style.button}>{children}</button>
-);
+export interface ButtonProps extends React.ComponentPropsWithoutRef<"button"> {
+  variant?: "contained" | "outlined" | "shadow";
+  color: "primary" | "secondary" | "tertiary" | "error";
+  StartIcon?: (params: any) => JSX.Element;
+  EndIcon?: (params: any) => JSX.Element;
+}
+
+const Button = ({
+  children,
+  variant = "contained",
+  color = "primary",
+  id,
+  onClick,
+  StartIcon,
+  EndIcon,
+  disabled = false,
+  className = "",
+  ...props
+}: ButtonProps) => {
+  return (
+    <button
+      disabled={disabled}
+      id={id}
+      onClick={onClick}
+      className={`${style.button} ${style[color]} ${style[variant]} ${className} `}
+      {...props}
+    >
+      {StartIcon != null && (
+        <StartIcon className={style.leftIcon} height="20" width="20" />
+      )}
+      {children}
+      {EndIcon != null && (
+        <EndIcon className={style.rightIcon} height="20" width="20" />
+      )}
+    </button>
+  );
+};
+
 export { Button };
