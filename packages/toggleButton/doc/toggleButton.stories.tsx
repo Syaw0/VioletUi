5fbe094357258d0427c79bbab8dc73005b4e9c62
Siavash <siamhb7@protonmail.com>
2023-02-24 18:31:58 +0330

    work on the toggle button styles.

--- packages/toggleButton/doc/toggleButton.stories.tsx
+++ packages/toggleButton/doc/toggleButton.stories.tsx
@@ -1,6 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 import { ToggleButton } from "../lib/toggleButton";
+import AlignCenter from "./alignCenter";
+import AlignLeft from "./alignLeft";
+import AlignRight from "./alignRight";
 
 export default { title: "ToggleButton" };
 
-export const toggleBtn = () => <ToggleButton />;
+export const toggleBtn = () => {
+  const [state, setState] = useState({
+    center: false,
+    left: false,
+    right: false,
+  });
+
+  const handle = (e: any, type: string) => {
+    setState((s) => ({ ...s, [type]: !s[type] }));
+  };
+
+  return (
+    <div style={{ display: "flex" }}>
+      <ToggleButton
+        onClick={(e) => handle(e, "center")}
+        selected={state.center}
+        value="center"
+      >
+        <AlignCenter />
+      </ToggleButton>
+
+      <ToggleButton
+        onClick={(e) => handle(e, "left")}
+        selected={state.left}
+        value="left"
+      >
+        <AlignLeft />
+      </ToggleButton>
+
+      <ToggleButton
+        onClick={(e) => handle(e, "right")}
+        selected={state.right}
+        value="right"
+      >
+        <AlignRight />
+      </ToggleButton>
+    </div>
+  );
+};
--- packages/toggleButton/lib/toggleButton.tsx
+++ packages/toggleButton/lib/toggleButton.tsx
@@ -2,9 +2,19 @@ import style from "./toggleButton.module.css";
 import React from "react";
 import "../../../t.css";
 
-interface ToggleButton extends React.ComponentPropsWithoutRef<"div"> {}
+interface ToggleButton extends React.ComponentPropsWithoutRef<"div"> {
+  selected: boolean;
+  value: string;
+}
 
-const ToggleButton = ({ color = "primary", ...props }: ToggleButton) => {
-  return <div className={style.holder}></div>;
+const ToggleButton = ({ selected = false, ...props }: ToggleButton) => {
+  return (
+    <div
+      {...props}
+      className={`${style.holder} ${selected ? style.selected : ""}`}
+    >
+      {props.children}
+    </div>
+  );
 };
 export { ToggleButton };
