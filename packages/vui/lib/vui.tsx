188d85f0cc5a4ecf7a9a7de458a99fc57a6df0cc
Siavash <siamhb7@protonmail.com>
2023-02-25 21:30:02 +0330

    add toggle group to vui.

--- packages/vui/lib/vui.tsx
+++ packages/vui/lib/vui.tsx
@@ -5,5 +5,15 @@ import { Alert } from "@violetui/alert";
 import { Tooltip } from "@violetui/tooltip";
 import { Badge } from "@violetui/badge";
 import { ToggleButton } from "@violetui/toggle_button";
+import { ToggleButtonGroup } from "@violetui/toggle_button_group";
 
-export { Button, Text, TextInput, Alert, Tooltip, Badge, ToggleButton };
+export {
+  Button,
+  Text,
+  TextInput,
+  Alert,
+  Tooltip,
+  Badge,
+  ToggleButton,
+  ToggleButtonGroup,
+};
