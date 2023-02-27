42b1cf98c64a2cf6efe45065311732b4fc4af587
Siavash <siamhb7@protonmail.com>
2023-02-27 19:13:53 +0330

    work on the icon button styles.

--- packages/iconButton/doc/iconButton.stories.js
+++ packages/iconButton/doc/iconButton.stories.js
@@ -1,9 +1,26 @@
 import React from "react";
 import { IconButton } from "../lib/iconButton";
 import "../../../t.css";
+import FingerPrintIcon from "./fingerPrintIcon";
 
 export default { title: "Icon Button" };
 
 export const iconButton1 = () => (
-  <IconButton color="primary">Hello Button</IconButton>
+  <>
+    <IconButton color="primary">
+      <FingerPrintIcon />
+    </IconButton>
+
+    <IconButton color="secondary">
+      <FingerPrintIcon />
+    </IconButton>
+
+    <IconButton color="tertiary">
+      <FingerPrintIcon />
+    </IconButton>
+
+    <IconButton color="error">
+      <FingerPrintIcon />
+    </IconButton>
+  </>
 );
