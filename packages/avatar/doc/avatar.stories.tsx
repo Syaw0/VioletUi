7b5420af44e8480e29b04cd3ab1d2a838e63dc45
Siavash <siamhb7@protonmail.com>
2023-02-28 20:44:21 +0330

    work on the variation of the avatar package.

--- packages/avatar/doc/avatar.stories.tsx
+++ packages/avatar/doc/avatar.stories.tsx
@@ -1,27 +1,43 @@
 import React from "react";
 import { Avatar } from "../lib/avatar";
+import FingerPrintIcon from "./fingerPrintIcon";
 
 export default { title: "Avatar" };
 
 export const avatar = () => (
-  <>
+  <div>
     <Avatar
       alt="img"
       src="https://user-images.githubusercontent.com/90524474/218326887-539649b7-a556-4214-a5a3-3d4e7e541bd2.jpg?w=128&q=50"
       size="large"
     ></Avatar>
-
     <Avatar alt="img" size="medium"></Avatar>
-
     <Avatar alt="img" size="medium">
       SS
     </Avatar>
     <Avatar alt="img" size="small">
       SS
     </Avatar>
-
     <Avatar alt="img" size="large" style={{ backgroundColor: "purple" }}>
       SS
     </Avatar>
-  </>
+    <Avatar alt="img" size="large">
+      <FingerPrintIcon />
+    </Avatar>
+    <Avatar alt="img" variant="square" size="large">
+      <FingerPrintIcon />
+    </Avatar>
+    <Avatar
+      alt="img"
+      variant="round"
+      src="https://user-images.githubusercontent.com/90524474/218326887-539649b7-a556-4214-a5a3-3d4e7e541bd2.jpg?w=128&q=50"
+      size="large"
+    ></Avatar>
+    <Avatar
+      alt="img"
+      variant="round"
+      src="https://user-images.githubusercontent.com/90524474/218326887-539649b7-a556-4214-a5a3-3d4e7e541bd2.jpg?w=128&q=50"
+      size="medium"
+    ></Avatar>
+  </div>
 );
--- packages/avatar/lib/avatar.tsx
+++ packages/avatar/lib/avatar.tsx
@@ -7,14 +7,21 @@ interface AvatarProps extends React.ComponentPropsWithoutRef<"div"> {
   alt?: string;
   src?: string;
   size?: "small" | "medium" | "large";
+  variant?: "round" | "square" | "circle";
 }
 
-const Avatar = ({ size = "medium", src, alt = "", ...props }: AvatarProps) => {
+const Avatar = ({
+  size = "medium",
+  variant = "circle",
+  src,
+  alt = "",
+  ...props
+}: AvatarProps) => {
   console.log(src == null, props.children == null);
   return (
     <div
       {...props}
-      className={`${style.holder} ${style[size]} ${
+      className={`${style.holder} ${style[size]} ${style[variant]} ${
         props.className != null ? props.className : ""
       }`}
     >
