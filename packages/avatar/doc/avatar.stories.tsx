7756b31b43f3180f7f11e20c1c580126f3788291
Siavash <siamhb7@protonmail.com>
2023-02-28 20:18:51 +0330

    work on the base of avatar package styles.

--- packages/avatar/doc/avatar.stories.tsx
+++ packages/avatar/doc/avatar.stories.tsx
@@ -1,111 +1,12 @@
 import React from "react";
-import { Alert } from "../lib/alert";
+import { Avatar } from "../lib/avatar";
 
-export default { title: "Alert" };
+export default { title: "Avatar" };
 
-export const Warning1 = () => (
-  <Alert
-    title="Alert"
-    type="warning"
-    onClose={(e) => {
-      console.log("hello friend!");
-    }}
-  >
-    You are disabling Cookies that mean may cause functionality problem!
-  </Alert>
-);
-
-export const Warning2 = () => (
-  <Alert title="Alert" type="warning">
-    You are disabling Cookies that mean may cause functionality problem!
-  </Alert>
-);
-
-export const Warning3 = () => (
-  <Alert title="Alert" variant="outlined" type="warning">
-    You are disabling Cookies that mean may cause functionality problem!
-  </Alert>
-);
-
-export const Warning4 = () => (
-  <Alert variant="filled" type="warning">
-    You are disabling Cookies that mean may cause functionality problem!
-  </Alert>
-);
-
-export const Success = () => (
-  <Alert variant="filled" type="success">
-    You are disabling Cookies that mean may cause functionality problem!
-  </Alert>
-);
-
-export const Success2 = () => (
-  <Alert variant="default" title="Success!" type="success">
-    You are disabling Cookies that mean may cause functionality problem!
-  </Alert>
-);
-
-export const Success3 = () => (
-  <Alert variant="outlined" type="success">
-    You are disabling Cookies that mean may cause functionality problem!
-  </Alert>
-);
-
-export const Success4 = () => (
-  <Alert variant="outlined" type="success" onClose={() => console.log("hello")}>
-    You are disabling Cookies that mean may cause functionality problem!
-  </Alert>
-);
-
-export const Error = () => (
-  <Alert variant="filled" type="error">
-    You are disabling Cookies that mean may cause functionality problem!
-  </Alert>
-);
-
-export const Error2 = () => (
-  <Alert variant="default" title="Error!" type="error">
-    You are disabling Cookies that mean may cause functionality problem!
-  </Alert>
-);
-
-export const Error3 = () => (
-  <Alert variant="outlined" type="error">
-    You are disabling Cookies that mean may cause functionality problem!
-  </Alert>
-);
-
-export const Error4 = () => (
-  <Alert variant="outlined" type="error" onClose={() => console.log("hello")}>
-    You are disabling Cookies that mean may cause functionality problem!
-  </Alert>
-);
-
-export const Info = () => (
-  <Alert variant="filled" type="info">
-    You are disabling Cookies that mean may cause functionality problem!
-  </Alert>
-);
-
-export const Info2 = () => (
-  <Alert variant="default" title="Info!" type="info">
-    You are disabling Cookies that mean may cause functionality problem!
-  </Alert>
-);
-
-export const Info4 = () => (
-  <Alert variant="outlined" type="info" title="Information!">
-    You are disabling Cookies that mean may cause functionality problem!
-  </Alert>
-);
-
-export const Info3 = () => (
-  <Alert
-    variant="outlined"
-    type="info"
-    title="Information!"
-    onClose={() => console.log("hello")}
-  >
-    You are disabling Cookies that mean may cause functionality problem!
-  </Alert>
+export const avatar = () => (
+  <Avatar
+    alt="img"
+    src="https://user-images.githubusercontent.com/90524474/218326887-539649b7-a556-4214-a5a3-3d4e7e541bd2.jpg?w=128&q=50"
+    size="large"
+  ></Avatar>
 );
--- packages/avatar/lib/avatar.tsx
+++ packages/avatar/lib/avatar.tsx
@@ -2,9 +2,29 @@ import style from "./avatar.module.css";
 import React from "react";
 import "../../../t.css";
 
-interface AvatarProps extends React.ComponentPropsWithoutRef<"div"> {}
+interface AvatarProps extends React.ComponentPropsWithoutRef<"div"> {
+  alt?: string;
+  src?: string;
+  size?: "small" | "medium" | "large";
+}
 
-const Avatar = ({ ...props }: AvatarProps) => {
-  return <div className={style.holder} {...props}></div>;
+const Avatar = ({
+  size = "medium",
+  src = "",
+  alt = "",
+  ...props
+}: AvatarProps) => {
+  return (
+    <div
+      {...props}
+      className={`${style.holder} ${style[size]} ${
+        props.className != null ? props.className : ""
+      }`}
+    >
+      {src != null && (
+        <img className={style.imgHolder} src={src} alt={alt}></img>
+      )}
+    </div>
+  );
 };
-export { Avatar as Alert };
+export { Avatar };
