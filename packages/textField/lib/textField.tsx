860ffb3e563ab7d53668700bfd4fbd0f06bd408c
Siavash <siamhb7@protonmail.com>
2023-03-01 23:15:25 +0330

    work on the label behavior in the text field package.

--- packages/textField/lib/textField.tsx
+++ packages/textField/lib/textField.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import style from "./textField.module.css";
 
 interface TextFieldType extends React.ComponentPropsWithoutRef<"input"> {
@@ -12,13 +12,57 @@ const TextField = ({
   label,
   ...props
 }: TextFieldType) => {
+  const [isLabelFocused, setIsLabelFocused] = useState(false);
+  const ref: any = useRef(null);
+  useEffect(() => {
+    const input = ref.current as HTMLInputElement;
+    if (document.activeElement == input || isLabelFocused) {
+      return;
+    }
+    if (input.value != "") {
+      setIsLabelFocused(true);
+    } else {
+      setIsLabelFocused(false);
+    }
+  });
+  const focusHandler = () => {
+    console.log("focus");
+    setIsLabelFocused(true);
+  };
+  const blurHandler = () => {
+    console.log("blur");
+    setIsLabelFocused(false);
+  };
+
+  const labelClickHandler = () => {
+    if (!isLabelFocused) {
+      setIsLabelFocused(true);
+      ref.current.focus();
+    }
+  };
   return (
     <div className={`${style.holder} `}>
+      {label != null && (
+        <label
+          onClick={labelClickHandler}
+          className={`${isLabelFocused ? style.label_focused : style.label} `}
+        >
+          {label}
+        </label>
+      )}
       <input
         {...props}
+        onFocus={(e) => {
+          props.onFocus && props.onFocus(e);
+          focusHandler();
+        }}
+        onBlur={(e) => {
+          props.onBlur && props.onBlur(e);
+          blurHandler();
+        }}
+        ref={ref}
         className={`${style.input} ${style[variant]} ${className}`}
       />
-      {label != null && <span className={style.label}>{label}</span>}
     </div>
   );
 };
