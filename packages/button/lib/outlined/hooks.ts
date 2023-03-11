ed36f295bac1357860ce3929d73de399365a0f12
Siavash <siamhb7@protonmail.com>
2023-03-11 19:44:39 +0330

    refactor focus event on outlined and filled tonal button .

--- packages/button/lib/outlined/hooks.ts
+++ packages/button/lib/outlined/hooks.ts
@@ -2,16 +2,11 @@ import makeBubbleSpan from "../shared/makeBubbleSpan";
 import {
   fadeHoverSpan,
   fadeHoverTiming,
-  fadePressSpan,
-  fadePressTiming,
-  growFocusSpan2,
-  growFocusTiming2,
   growHoverSpan,
   growHoverTiming,
-  growPressSpan,
-  growPressTiming,
 } from "../animations";
 import onmousedown from "../shared/onmousedown";
+import onfocus from "../shared/onfocus";
 
 const useElevatedEvents = (
   isDisabled: boolean,
@@ -26,32 +21,7 @@ const useElevatedEvents = (
   };
 
   const handleFocus = () => {
-    if (isDisabled || isClicked) {
-      return;
-    }
-    const button = btn.current as HTMLButtonElement;
-    const span = makeBubbleSpan(0, 0, "50%", -20);
-    button.appendChild(span);
-    span.style.borderRadius = "30px";
-    span.style.width = "37%";
-    span.style.height = "30px";
-    span.animate(growPressSpan, growPressTiming);
-    span.animate(growFocusSpan2, growFocusTiming2);
-    const blur = () => {
-      button.removeEventListener("blur", blur);
-      if (isDisabled || isClicked) {
-        return;
-      }
-      const fadeBubbleAnimation = new Animation(
-        new KeyframeEffect(span, fadePressSpan, fadePressTiming),
-        document.timeline
-      );
-      fadeBubbleAnimation.play();
-      fadeBubbleAnimation.addEventListener("finish", () => {
-        button.removeChild(span);
-      });
-    };
-    button.addEventListener("blur", blur);
+    onfocus(isDisabled, isClicked, btn, 0, 0);
   };
 
   const handleHover = () => {
